6af172ea7af48a00ab449e6becfaaf1df5d855dc
HwangSunNam <hwang67120@gmail.com>
2026-05-16 17:19:19 +0900

    initial commit

---
+++ src/main/java/com/example/olca/OlcaApplication.java
@@ -0,0 +1,13 @@
+package com.example.olca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/olca/OlcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
